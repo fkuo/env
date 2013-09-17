118e46c7ecc0f1b9b17d420e33c4beb13d09ba9a
Fred Kuo <fred.kuo@joyent.com>
2013-09-17 18:01:35 +0000

    better copy in and copy out

---
+++ copyin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function copyifdiff {
+    diff $1 $2
+    if [ $? -ne 0 ]; then
+        cp $1 $2
+    fi
+}
+
+copyifdiff ~/.gitconfig gitconfig
+copyifdiff ~/.zhsrc zshrc
+copyifdiff ~/.oh-my-zsh/themes/fred.zsh-theme fred.zsh-theme
+copyifdiff ~/.profile profile
+copyifdiff ~/.vimrc vimrc
---
+++ copyout.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function copyifdiff {
+    diff $1 $2
+    if [ $? -ne 0 ]; then
+        cp $1 $2
+    fi
+}
+
+copyifdiff gitconfig ~/.gitconfig
+copyifdiff zshrc ~/.zshrc
+copyifdiff fred.zsh-theme ~/.oh-my-zsh/themes/fred.zsh-theme
+copyifdiff profile ~/.profile
+copyifdiff vimrc ~/.vimrc
